4744e8b1ab4f14f53b7cdec28fc461908adea796
bstephe9 <bstephenson9807@gmail.com>
2025-05-11 16:01:25 -0400

    chore: Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://bstephe9.github.io/to-do-list/",
+  server: {
+    open: true,
+  },
 })
